e14806c06882464d9fe0cce7eeef8b57281381b5
EndyKaufman <admin@site15.ru>
2016-02-25 23:13:03 +0500

    start add work with grid

---
+++ src/js/file/file.ctrl.js
@@ -0,0 +1,8 @@
+app.controller('ProjectCtrl', function ($scope, $timeout, ProjectSvc, AccountSvc, TagSvc, FileSvc) {
+    $scope.AccountSvc=AccountSvc;
+	$scope.ProjectSvc=ProjectSvc;
+	$scope.TagSvc=TagSvc;
+	$scope.FileSvc=FileSvc;
+
+	ProjectSvc.init();
+});
