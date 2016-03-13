7f36e22057399b0277260dff264a5a5d01f59b68
EndyKaufman <admin@site15.ru>
2016-03-13 23:38:53 +0500

    strat add new template

--- src/js/app.const.js
+++ src/js/app.const.js
@@ -41,6 +41,7 @@ app.factory('AppConst', function($rootScope, AccountConst, TagConst, NoteConst, 
     var home={
         url:'#/home',
         title: 'MY BLOG',
+        description: 'description of blog',
         name: 'MY_BLOG',
         image: '//2.gravatar.com/avatar/767fc9c115a1b989744c755db47feb60?s=132&d=wavatar'
     };
---
+++ src/theme/future-imperfect/js/app.theme.js
@@ -0,0 +1,15 @@
+app.config(function ($selectProvider, showErrorsConfigProvider, $carouselProvider, $modalProvider) {
+    showErrorsConfigProvider.showSuccess(true);
+
+    var mydefaults = {
+        outerWidth:'100%',
+        //innerHeight:'350px',
+        interval:15000,
+    }
+    angular.extend($carouselProvider.defaults, mydefaults);
+
+    var mydefaults = {
+        backdrop: false
+    }
+    angular.extend($modalProvider.defaults, mydefaults);
+});
